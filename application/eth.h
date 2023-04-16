8a79f64ab2d481babb4585fab9f8ffbcc125ca0e
Zero0Hero <64831522+Zero0Hero@users.noreply.github.com>
2023-04-16 11:29:33 +0800

    Delete application directory

--- application/eth.h
+++
@@ -1,7 +0,0 @@
-
-void nice_eth_on();
-void nice_eth_off();
-void nice_eth_udp_len(unsigned len, unsigned fre);
-
-void eth_test();
-void eth_once();
--- application/video.h
+++
@@ -1,7 +0,0 @@
-
-void nice_cam_on();
-void nice_cam_off();
-
-void nice_hdmi_on();
-void nice_hdmi_off();
-
